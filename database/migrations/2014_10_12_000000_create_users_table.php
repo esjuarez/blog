b5cb3f80bca00351868fd9b6c224e89acc3017b2
esjuarez <es.juarez@mavi.mx>
2023-05-25 20:40:35 +0200

    Setup user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->uuid('id')->unique()->primary();
+            $table->string('name', 50);
+            $table->string('email', 80)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role', 10);
             $table->rememberToken();
             $table->timestamps();
         });
